bb0a8f6313d059b8b535ef0c21548f5c89a1dd70
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-07-09 00:08:06 +0200

    first steps at reworking pages

--- WheelWizard/Views/Pages/ModsPage.xaml.cs
+++ WheelWizard/Views/Pages/ModsPage.xaml.cs
@@ -21,7 +21,8 @@ namespace CT_MKWII_WPF.Views.Pages
             Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
             "CT-MKWII", "Mods", "modconfig.json");
         public ObservableCollection<Mod> Mods { get; set; }
-        
+
+        private bool _toggleAll = true;
         private Point startPoint;
         public ModsPage()
         {
@@ -55,8 +56,9 @@ namespace CT_MKWII_WPF.Views.Pages
         
         private void UpdateEmptyListMessageVisibility()
         {
-            EmptyListMessage.Visibility = Mods.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
-            ModsListView.Visibility = Mods.Count > 0 ? Visibility.Visible : Visibility.Collapsed; 
+            PageWithoutMods.Visibility = Mods.Count == 0 ? Visibility.Visible : Visibility.Collapsed;
+            TopBarButtons.Visibility = Mods.Count > 0 ? Visibility.Visible : Visibility.Collapsed;
+            PageWithMods.Visibility = Mods.Count > 0 ? Visibility.Visible : Visibility.Collapsed; 
         }
         
         private void Mod_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -264,9 +266,13 @@ namespace CT_MKWII_WPF.Views.Pages
 
         private void EnableClick(object sender, RoutedEventArgs e)
         {
-            bool selectAll = Mods.Any(mod => !mod.IsEnabled);
+            //bool selectAll = Mods.Any(mod => !mod.IsEnabled);
             foreach (var mod in Mods)
-                mod.IsEnabled = selectAll;
+                mod.IsEnabled = _toggleAll;
+            _toggleAll = !_toggleAll;
+            if (!_toggleAll)
+                EnableDisableButton.Text = "Disable All";
+            else EnableDisableButton.Text = "Enable All";
         }
 
         private bool IsValidName(string name)
