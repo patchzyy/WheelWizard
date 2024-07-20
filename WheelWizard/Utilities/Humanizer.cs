487bcfffeb2f53cfc2e8450f3efaf460830d8b13
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-07-20 18:07:44 +0200

    make humanizer absolute

--- WheelWizard/Utilities/Humanizer.cs
+++ WheelWizard/Utilities/Humanizer.cs
@@ -17,16 +17,16 @@ public static class Humanizer
         
     public static string HumanizeTimeSpan(TimeSpan timeSpan)
     {
-        if (timeSpan.TotalDays >= 1)
-            return $"{timeSpan.Days} day{P(timeSpan.Days)} {timeSpan.Hours} hour{P(timeSpan.Hours)}";
-        if (timeSpan.TotalHours >= 1)
-            return $"{timeSpan.Hours} hour{P(timeSpan.Hours)} {timeSpan.Minutes} minute{P(timeSpan.Minutes)}";
-        if (timeSpan.TotalMinutes >= 1)
-            return $"{timeSpan.Minutes} minute{P(timeSpan.Minutes)} {timeSpan.Seconds} second{P(timeSpan.Seconds)}";
+        if (Math.Abs(timeSpan.TotalDays) >= 1)
+            return $"{timeSpan.Days} day{P(timeSpan.Days)} {Math.Abs(timeSpan.Hours)} hour{P(timeSpan.Hours)}";
+        if (Math.Abs(timeSpan.TotalHours) >= 1)
+            return $"{timeSpan.Hours} hour{P(timeSpan.Hours)} {Math.Abs(timeSpan.Minutes)} minute{P(timeSpan.Minutes)}";
+        if (Math.Abs(timeSpan.TotalMinutes) >= 1)
+            return $"{timeSpan.Minutes} minute{P(timeSpan.Minutes)} {Math.Abs(timeSpan.Seconds)} second{P(timeSpan.Seconds)}";
 
         return $"{timeSpan.Seconds} second{P(timeSpan.Seconds)}";
 
         // internal method to simplify the pluralization of words
-        string P(int count) => count != 1 ? "s" : "";
+        string P(int count) => Math.Abs(count) != 1 ? "s" : "";
     }
 }
