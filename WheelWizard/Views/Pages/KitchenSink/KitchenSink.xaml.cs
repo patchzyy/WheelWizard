8718d541bb6cf5e7c7c198bafcf9fb5fb9623d36
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-07-22 13:08:06 +0200

    adding different kitchen sink tabs

--- WheelWizard/Views/Pages/KitchenSink/KitchenSink.xaml.cs
+++ WheelWizard/Views/Pages/KitchenSink/KitchenSink.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace CT_MKWII_WPF.Views.Pages.KitchenSink;
@@ -8,5 +9,21 @@ public partial class KitchenSink : Page
     public KitchenSink()
     {
         InitializeComponent();
+        KitchenSinkContent.Content = new KsGeneric();
+    }
+    
+    private void TopBarRadio_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (sender is not RadioButton radioButton) return;
+        
+        // As long as the Ks... files are next to this file, it works. 
+        var namespaceName = GetType().Namespace;
+        var typeName = $"{namespaceName}.{radioButton.Tag}";
+        var type = Type.GetType(typeName);
+        if (type == null || !typeof(UserControl).IsAssignableFrom(type)) return;
+
+        if (Activator.CreateInstance(type) is not UserControl instance) return;
+        
+        KitchenSinkContent.Content = instance;
     }
 }
---
+++ WheelWizard/Views/Pages/KitchenSink/KsGeneric.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CT_MKWII_WPF.Views.Pages.KitchenSink;
+
+public partial class KsGeneric : UserControl
+{
+    public KsGeneric()
+    {
+        InitializeComponent();
+    }
+}
+
