8a74617656d35446b679ea6a660c64a1a9d82f57
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-09-07 12:38:40 +0200

    white space

--- WheelWizard/Views/Pages/KitchenSink/KitchenSink.xaml.cs
+++ WheelWizard/Views/Pages/KitchenSink/KitchenSink.xaml.cs
@@ -14,15 +14,18 @@ public partial class KitchenSink : Page
     
     private void TopBarRadio_OnClick(object sender, RoutedEventArgs e)
     {
-        if (sender is not RadioButton radioButton) return;
+        if (sender is not RadioButton radioButton) 
+            return;
         
         // As long as the Ks... files are next to this file, it works. 
         var namespaceName = GetType().Namespace;
         var typeName = $"{namespaceName}.{radioButton.Tag}";
         var type = Type.GetType(typeName);
-        if (type == null || !typeof(UserControl).IsAssignableFrom(type)) return;
+        if (type == null || !typeof(UserControl).IsAssignableFrom(type)) 
+            return;
 
-        if (Activator.CreateInstance(type) is not UserControl instance) return;
+        if (Activator.CreateInstance(type) is not UserControl instance) 
+            return;
         
         KitchenSinkContent.Content = instance;
     }
