64912f26e69cea901060f138a931cdc2ffef1d6e
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-11-16 00:16:26 +0100

    thats how

--- WheelWizard/Program.cs
+++ WheelWizard/Program.cs
@@ -22,4 +22,11 @@ public class Program
                      .UsePlatformDetect()
                      .WithInterFont()
                      .LogToTrace();
-}
+    
+    // TODO: Complete the avalonia transition
+    //  - When the whole avalonia views are complete and work, remove the WPFviews folder.
+    //  - Then go in the csproj, and replace `   <TargetFramework>net7.0-windows</TargetFramework>` with `net7.0` or `net8.0` (just not with the -windows)
+    //  - Then remove the `<UseWPF>true</UseWPF>` tag
+    //  - Then go int the <ItemGroup> that has the comments about something specific to the OG Wheel wizard wpf application. Filter everything in there what should all be thrown away
+    //  - Remove all the page xaml references in this .csproj
+} 
