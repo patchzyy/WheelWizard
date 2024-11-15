fd415dda98de378e27e0d8b2c73ebb34de11f7ba
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-11-16 00:21:51 +0100

    make them threaded so that we can have them next to each other

--- WheelWizard/Program.cs
+++ WheelWizard/Program.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using System;
+using System.Threading;
 
 namespace WheelWizard;
 
@@ -10,11 +11,23 @@ public class Program
     {
         Console.WriteLine("Application start");
         
-        var app = new WheelWizard.App();
-        app.InitializeComponent();
-        app.Run();
+        var wpfThread = new Thread(() =>
+                   {
+                       var wpfApp = new App(); // WPF App
+                       wpfApp.InitializeComponent();
+                       wpfApp.Run();
+                   });
+        wpfThread.SetApartmentState(ApartmentState.STA); 
+     
         
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        var avaloniaThread = new Thread(() =>
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        });
+        avaloniaThread.SetApartmentState(ApartmentState.STA); 
+        
+        avaloniaThread.Start();
+        wpfThread.Start();
     }
     
     public static AppBuilder BuildAvaloniaApp()
@@ -25,6 +38,7 @@ public class Program
     
     // TODO: Complete the avalonia transition
     //  - When the whole avalonia views are complete and work, remove the WPFviews folder.
+    //  - Remove the 2 threads in the main method, instead it should only have the avalonia app without the thread
     //  - Then go in the csproj, and replace `   <TargetFramework>net7.0-windows</TargetFramework>` with `net7.0` or `net8.0` (just not with the -windows)
     //  - Then remove the `<UseWPF>true</UseWPF>` tag
     //  - Then go int the <ItemGroup> that has the comments about something specific to the OG Wheel wizard wpf application. Filter everything in there what should all be thrown away
