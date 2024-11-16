ce9b6f90734cccf788e176b8b38ea3d840d2f70b
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-11-16 15:12:51 +0100

    layout

--- WheelWizard/Views/App.axaml.cs
+++ WheelWizard/Views/App.axaml.cs
@@ -15,7 +15,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow();
+            desktop.MainWindow = new Layout();
         }
 
         base.OnFrameworkInitializationCompleted();
