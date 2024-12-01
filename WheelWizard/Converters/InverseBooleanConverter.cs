788fb4fb0010764f620ba16ab06eeb622dba326a
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-12-01 22:21:54 +0100

    Finally worked out how a component works in avalonia

---
+++ WheelWizard/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace WheelWizard.Converters;
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolean)
+            return !boolean;
+        return AvaloniaProperty.UnsetValue;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool boolean)
+            return !boolean;
+        return AvaloniaProperty.UnsetValue;
+    }
+}
---
+++ WheelWizard/Views/Components/IconLabel.axaml.cs
@@ -0,0 +1,56 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace WheelWizard.Views.Components;
+
+public class IconLabel : TemplatedControl
+{
+    public static readonly StyledProperty<Geometry> IconDataProperty =
+        AvaloniaProperty.Register<IconLabel,Geometry>(nameof(IconData));
+   
+    public Geometry IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+    
+    public static readonly StyledProperty<double> IconSizeProperty =
+        AvaloniaProperty.Register<IconLabel,double>(nameof(IconSize));
+   
+    public double IconSize
+    {
+        get => GetValue(IconSizeProperty);
+        set => SetValue(IconSizeProperty, value);
+    }
+    
+    public static readonly StyledProperty<object> TextProperty =
+        AvaloniaProperty.Register<IconLabel, object>(nameof(Text));
+    
+    public object Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    
+    
+    public static readonly StyledProperty<bool> IsIconLeftProperty =
+        AvaloniaProperty.Register<IconLabel, bool>(nameof(IsIconLeft), true);
+
+    public bool IsIconLeft
+    {
+        get => GetValue(IsIconLeftProperty);
+        set => SetValue(IsIconLeftProperty, value);
+    }
+    
+    public static readonly StyledProperty<bool> IsUnderlinedProperty =
+        AvaloniaProperty.Register<IconLabel, bool>(nameof(IsUnderlined), false);
+
+    public bool IsUnderlined
+    {
+        get => GetValue(IsUnderlinedProperty);
+        set => SetValue(IsUnderlinedProperty, value);
+    }
+}
+
