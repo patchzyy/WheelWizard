2bfd28115df242394dafe0756b413c862a55b0f6
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-12-06 23:58:03 +0100

    label button

---
+++ WheelWizard/Views/Components/IconLabelButton.axaml.cs
@@ -0,0 +1,30 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Media;
+using System;
+
+namespace WheelWizard.Views.Components;
+
+public class IconLabelButton : IconLabel
+{
+    public static readonly StyledProperty<IBrush> HoverForegroundProperty =
+        AvaloniaProperty.Register<IconLabelButton, IBrush>(nameof(HoverForeground));
+
+    public IBrush HoverForeground
+    {
+        get => GetValue(HoverForegroundProperty);
+        set => SetValue(HoverForegroundProperty, value);
+    }
+    
+    public event EventHandler? Click;
+    
+    protected override void OnPointerPressed(PointerPressedEventArgs e)
+    {
+        base.OnPointerPressed(e);
+        if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            Click?.Invoke(this, EventArgs.Empty);
+    }
+}
+
