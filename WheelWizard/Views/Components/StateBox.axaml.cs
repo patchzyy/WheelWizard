229ebbd47977b444f124d13d0cfdea218ab5e50a
patchzyy <64382339+patchzyy@users.noreply.github.com>
2024-12-09 15:08:29 +0100

    Started work on Radio buttons

---
+++ WheelWizard/Views/Components/StateBox.axaml.cs
@@ -0,0 +1,68 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace WheelWizard.Views.Components;
+
+public partial class StateBox : UserControl
+{
+    public StateBox()
+    {
+        InitializeComponent();
+    }
+
+    // Added Text property
+    public static readonly StyledProperty<string> TextProperty =
+        AvaloniaProperty.Register<StateBox, string>(nameof(Text));
+
+    public string Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    
+    public static readonly StyledProperty<Geometry> IconDataProperty =
+        AvaloniaProperty.Register<StateBox, Geometry>(nameof(IconData));
+   
+    public Geometry IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+    
+    public static readonly StyledProperty<double> IconSizeProperty =
+        AvaloniaProperty.Register<StateBox, double>(nameof(IconSize), 14);
+   
+    public double IconSize
+    {
+        get => GetValue(IconSizeProperty);
+        set => SetValue(IconSizeProperty, value);
+    }
+    
+    public static readonly StyledProperty<object> TipTextProperty =
+        AvaloniaProperty.Register<StateBox, object>(nameof(TipText), "Tip goes here");
+   
+    public object TipText
+    {
+        get => GetValue(TipTextProperty);
+        set => SetValue(TipTextProperty, value);
+    }
+    
+    public static readonly StyledProperty<PlacementMode> TipPlacementProperty =
+        AvaloniaProperty.Register<StateBox, PlacementMode>(nameof(TipPlacement), PlacementMode.Right);
+
+    public PlacementMode TipPlacement
+    {
+        get => GetValue(TipPlacementProperty);
+        set => SetValue(TipPlacementProperty, value);
+    }
+    
+    public static readonly StyledProperty<double> FontSizeProperty =
+        AvaloniaProperty.Register<StateBox, double>(nameof(FontSize), 12);
+   
+    public double FontSize
+    {
+        get => GetValue(FontSizeProperty);
+        set => SetValue(FontSizeProperty, value);
+    }
+}
---
+++ WheelWizard/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WheelWizard.Views.Pages;
+
+public partial class HomePage : UserControl
+{
+    public HomePage()
+    {
+        InitializeComponent();
+    }
+}
+
