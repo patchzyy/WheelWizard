e1f3758c8e8db2b46b020c1b72e35155fbc4db85
patchzyy <64382339+patchzyy@users.noreply.github.com>
2024-12-12 12:58:18 +0100

    Update!

--- WheelWizard/Services/Installation/AutoUpdater.cs
+++ WheelWizard/Services/Installation/AutoUpdater.cs
@@ -16,7 +16,7 @@ namespace WheelWizard.Services.Installation;
 
 public static class AutoUpdater
 {
-    public const string CurrentVersion = "1.8.5";
+    public const string CurrentVersion = "1.8.6";
     public static async Task CheckForUpdatesAsync()
     {
         var response = await HttpClientHelper.GetAsync<string>(Endpoints.WhWzLatestReleasesUrl);
