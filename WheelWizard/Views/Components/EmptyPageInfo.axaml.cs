513d59f5bd790eb82ab4c596cb27f3d5f40e8842
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-12-15 12:21:13 +0100

    Empty Page Info

---
+++ WheelWizard/Views/Components/EmptyPageInfo.axaml.cs
@@ -0,0 +1,38 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace WheelWizard.Views.Components;
+
+public class EmptyPageInfo : TemplatedControl
+{
+    public static readonly StyledProperty<Geometry> IconDataProperty =
+        AvaloniaProperty.Register<EmptyPageInfo,Geometry>(nameof(IconData));
+   
+    public Geometry IconData
+    {
+        get => GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+
+    
+    public static readonly StyledProperty<string> BodyTextProperty =
+        AvaloniaProperty.Register<EmptyPageInfo, string>(nameof(BodyText), "It seems like there is no content to  display here.");
+    
+    public string BodyText
+    {
+        get => GetValue(BodyTextProperty);
+        set => SetValue(BodyTextProperty, value);
+    }
+    
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<EmptyPageInfo, string>(nameof(Title), "This is empty");
+    
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+}
+
