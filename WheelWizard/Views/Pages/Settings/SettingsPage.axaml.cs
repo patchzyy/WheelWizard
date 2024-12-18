a5012f27611141150dccadf80f9180ad8610fec8
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2024-12-18 22:48:31 +0100

    setting up the settings page

---
+++ WheelWizard/Views/Pages/Settings/SettingsPage.axaml.cs
@@ -0,0 +1,49 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+using WheelWizard.Services.Installation;
+
+namespace WheelWizard.Views.Pages.Settings;
+
+public partial class SettingsPage : UserControl
+{
+    public SettingsPage() : this(new WhWzSettings()) { }
+    public SettingsPage(UserControl initialSettingsPage)
+    {
+        InitializeComponent();
+        
+        WhWzVersionText.Text = "WhWz: v" + AutoUpdater.CurrentVersion;
+        RrVersionText.Text = "RR: " + RetroRewindInstaller.CurrentRRVersion();
+        
+    #if RELEASE_BUILD
+        ReleaseText.IsVisible = false;
+        DevButton.IsVisible = false;
+    #endif
+        
+        SettingsContent.Content = initialSettingsPage;
+    }
+
+    private void TopBarRadio_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not RadioButton radioButton) 
+            return;
+        
+        // As long as the Ks... files are next to this file, it works. 
+        var namespaceName = GetType().Namespace;
+        var typeName = $"{namespaceName}.{radioButton.Tag}";
+        var type = Type.GetType(typeName);
+        if (type == null || !typeof(UserControl).IsAssignableFrom(type)) 
+            return;
+
+        if (Activator.CreateInstance(type) is not UserControl instance) 
+            return;
+        
+        SettingsContent.Content = instance;
+    }
+    
+    private void DevButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        // This is the button at the top right, that opens the dev tool that we can put ourown stuff in
+    }
+}
+
---
+++ WheelWizard/Views/Pages/Settings/WhWzSettings.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WheelWizard.Views.Pages.Settings;
+
+public partial class WhWzSettings : UserControl
+{
+    public WhWzSettings()
+    {
+        InitializeComponent();
+    }
+}
+
