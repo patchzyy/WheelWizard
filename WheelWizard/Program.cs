76984e67410fd7af659bd5464622324b65e4297f
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-02-05 13:37:15 +0100

    remove the comments

--- WheelWizard/Program.cs
+++ WheelWizard/Program.cs
@@ -28,14 +28,4 @@ public class Program
         AutoUpdater.CheckForUpdatesAsync();
         UrlProtocolManager.SetWhWzSchemeAsync();
     }
-
-    // TODO: Complete the avalonia transition
-    //  - When the whole avalonia views are complete and work, remove the WPFviews folder.
-    //  - Remove the 2 threads in the main method, instead it should only have the avalonia app without the thread
-    //  - Then go in the csproj, and replace `   <TargetFramework>net7.0-windows</TargetFramework>` with `net7.0` or `net8.0` (just not with the -windows)
-    //  - Then remove the `<UseWPF>true</UseWPF>` tag
-    //  - Then go int the <ItemGroup> that has the comments about something specific to the OG Wheel wizard wpf application. Filter everything in there what should all be thrown away
-    //  - Remove all the page xaml references in this .csproj
-    //  - Check what AssemblyInfo.cs is doing there still without WPF
-    //  - Move the new App.axaml in the place of App.xaml (in the root directory)
 } 
