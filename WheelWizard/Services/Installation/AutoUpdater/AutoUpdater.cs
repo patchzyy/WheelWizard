d9eed126b82d499b00913e411313def03c43efa0
WantToBeeMe <93130991+WantToBeeMe@users.noreply.github.com>
2025-02-18 17:59:05 +0100

    create a fallback updator

--- WheelWizard/Services/Installation/AutoUpdater/AutoUpdater.cs
+++ WheelWizard/Services/Installation/AutoUpdater/AutoUpdater.cs
@@ -7,18 +7,14 @@ public class AutoUpdater
     public const string CurrentVersion = "2.0.0";
     public static async void CheckForUpdatesAsync()
     {
-        IUpdaterPlatform? autoUpdaterPlatform = null;
+        IUpdaterPlatform autoUpdaterPlatform = new AutoUpdaterFallback();
 #if WINDOWS
-            autoUpdaterPlatform = new AutoUpdaterWindows();
-#elif LINUX
-            autoUpdaterPlatform = new AutoUpdaterLinux();
-#elif MACOS
-            // MacOS updater
-#elif DEBUG
         autoUpdaterPlatform = new AutoUpdaterWindows();
+#elif LINUX
+        autoUpdaterPlatform = new AutoUpdaterLinux();
+#elif MACOS
+        // MacOS updater
 #endif
-        if (autoUpdaterPlatform == null)
-            throw new PlatformNotSupportedException("The current platform is not supported by the auto updater.");
         IAutoUpdateHandler updateHandler = new AutoUpdaterHandler(autoUpdaterPlatform);
         
         //todo: how to run this in a background thread?
---
+++ WheelWizard/Services/Installation/AutoUpdater/AutoUpdaterFallback.cs
@@ -0,0 +1,32 @@
+﻿using Semver;
+using System.Threading.Tasks;
+using WheelWizard.Models.Github;
+using WheelWizard.Views.Popups.Generic;
+
+
+namespace WheelWizard.Services.Installation.AutoUpdater;
+
+public class AutoUpdaterFallback : IUpdaterPlatform
+{
+    public GithubAsset? GetAssetForCurrentPlatform(GithubRelease release)
+    {
+        var latestVersion = SemVersion.Parse(release.TagName.TrimStart('v'), SemVersionStyles.Any);
+        var currentVersion = SemVersion.Parse(AutoUpdater.CurrentVersion, SemVersionStyles.Any);
+        if (currentVersion.ComparePrecedenceTo(latestVersion) >= 0) return null;
+
+        Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(() =>
+        {
+            new MessageBoxWindow()
+                .SetTitleText("New Wheel Wizard version")
+                .SetInfoText("There is a new Wheel Wizard version available!\n" +
+                             $"Version {release.TagName.TrimStart('v')} (You are currently on {AutoUpdater.CurrentVersion})\n" +
+                             "You can manually update it by going to the github releases at: " +
+                             "https://github.com/patchzyy/WheelWizard/releases")
+                .Show();
+        });
+        
+        return null;
+    }
+
+    public Task ExecuteUpdateAsync(string downloadUrl) => Task.CompletedTask;
+}
